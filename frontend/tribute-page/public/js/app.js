4fdc17c2831927010f5a515ef6a7d34d7703b578
Peter Martinson <peter.j.martinson@gmail.com>
2017-04-20 22:31:21 -0400

    Wrote a real tribute to Seilacher, and did some styling

--- frontend/tribute-page/public/js/app.js
+++ frontend/tribute-page/public/js/app.js
@@ -1,5 +1,6 @@
 var output = '<section>' +
 '<h1>Adolf Seilacher</h1>' +
+'<span>(March 15, 1925 - April 26, 2014)</span>' +
 '<h2>Linked the deep ocean with prehistoric life</h2>' +
 '</section>' +
 '<article>' +
@@ -10,23 +11,19 @@ var output = '<section>' +
 '    <p>Modern paleodictyon found on bottom of the ocean, by Seilacher</p>' +
 '  </section>' +
 '  <section>' +
-'    <ul>' +
-'      <li>Seilacher\'s publications are numerous (well over 200) and cover a range of topics.</li>' +
-'      <li>His studies on trace fossils are perhaps his best-known contributions, especially his 1967 work on the bathymetry of trace fossils.</li>' +
-'      <li>He established the concept of ichnofacies: distinctive assemblages of trace fossils controlled largely by depth.</li>' +
-'      <li>This characterisation was later expanded to include the influences of substrate, oxygen, salinity and so on. In addition, he analysed many trace fossils in terms of the behaviour they represent, leading to such work as early computer simulation of trace fossil morphology (with David Raup, in 1969).</li>' +
-'      <li>Much of this work is summarized together with new material in Trace Fossil Analysis (2007).</li>' +
-'      <li>In 1970 he announced his programme of "Konstruktions-Morphologie" where he stressed the importance of three factors in determining the form of organisms:<br>' +
-'        <ol>' +
-'          <li>ecological/adaptive aspects</li>' +
-'          <li>historical/phylogenetic aspects</li>' +
-'          <li>architectural/constructional aspects</li>' +
-'        </ol>' +
-'      </li>' +
-'    </ul>' +
-'  </section>' +
-'  <section>' +
-'    <p> For more, go to <a href="https://stonetelescope.wordpress.com/2015/02/03/life-before-extinction-seilachers-theory-of-the-golden-ages/">my research paper on Seilacher</a>.</p>' +
+'    <p>It is said that old warriors never die, they just fade away.  In a similar way, old paleontologists simply enter geological time.  Adolf Seilacher was one of the old guard of paleontology when he became part of the fossil record.  In a time when young hotshot paleontologists can do a few fast Fourier transforms on vast databases of fossil data, and generate fantastical New York Times headlines, Seilacher <strong>worked</strong>.  His breakthroughs were rarely the stuff of clickbait, but his work is now foundational in paleontology courses.</p>' +
+'    <p>Late in life, Seilacher announced his theory of the "Golden Ages".  It is well known that the dinosaurs were done in by an incredible catastrophe, usually attributed to an asteroid or volcanic outbursts.  That extinction was massive, but the Earth has seen many extinctions at a smaller scale.  Seilacher observed that, leading up to extinctions, those species that ceased to exist had developed both strange morphologies and strange behaviors.  For example, before going extinct, many species tend to grow gigantic.  Or, they grow strange appendages, like the enormous antlers on the Irish Elk.  In Seilacher\'s terms, a species <em>grows decadent</em>.</p>' +
+'    <p>Seilacher saw this phenomenon as a natural result of Darwinian natural selection.  As organisms out compete each other, they tend to become ever more specialized in their niche.  Their specializations are manifestations of what gives them an advantage over other competing creatures.  Over time, organisms grow <em>overspecialized</em>.  They become so finely tuned to their niche that even small changes in the environment can throw them off their game.  If their environment changes quickly enough, their specializations could become burdens.  Evolution would then favor less developed species, who don\'t have to first back out of previous specializations, before adapting to new conditions.  In this way, decadent species become prime targets for extinction.</p>' +
+'    <p>Seilacher observed that there are periods when many species tend to grow decadent together.  During these <strong>Golden Ages</strong>, the Earth\'s climate remained relatively constant, providing no real challenges to evolution.  When catastrophe hit, however, all of these decadent lineages disappeared.  Wiped out, by their own success.  These are the mass extinction events.  Seilacher claimed that, before every major extinction, there would exist a Golden Age.</p>' +
+'    <p>Seilacher made two claims as a result of his theory, one which was proven true, and one which is now a major area of research.</p>' +
+'    <ol>' +
+'      <li>Overspecialization occurs in environments that are shielded from rapid change.  One place on the Earth that fits this description happens to be the bottom of the ocean.  The ocean floor does not experience a day/night cycle, does not feel the tides, and is unaffected by changes in climate.  Seilacher declared that at the bottom of the ocean, we would find fantastical creatures, so overspecialized as to be unrecognizable.  We might also find creatures long thought extinct, doing today exactly what their fossils showed them doing millions of years ago.  He was right on both counts.  Seilacher himself traveled down to the ocean floor once, and observed such a "living fossil", pictured above.</li>' +
+'      <li>Since mass extinctions usually followed extended terrestrial hothouse conditions, Seilacher suggested that "the ball needs to be tossed to the astronomers."  What galactic phenomena drive climate cycles on the Earth?  What causes prolonged hothouse conditions here?  Is it perhaps changes in cosmic ray intensity, which itself is changed by both solar activity, and by our solar system\'s motion through the galaxy?  How does the Earth\'s interaction with its intergalactic neighborhood impact the ability for creatures to become overspecialized?</li>' +
+'    </ol>' +
+'    <p>The concept of Golden Ages is only one of Seilacher\'s many contributions to not just paleontology, but to Man\'s understanding of his place in the universe.  The rocks may now have his bones, but his ideas are ours.</p>' +
+// '  </section>' +
+// '  <section>' +
+'    <p> For more, please see <a href="https://stonetelescope.wordpress.com/2015/02/03/life-before-extinction-seilachers-theory-of-the-golden-ages/">my research paper on Seilacher</a>.</p>' +
 '  </section>' +
 '</article>';
 
