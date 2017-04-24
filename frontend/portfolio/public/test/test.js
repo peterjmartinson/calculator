327e8d6d8656a492d4571bfbf6758dc23a7fcd7a
Peter Martinson <peter.j.martinson@gmail.com>
2017-04-23 23:07:14 -0400

    Add first test - fails, as planned!

--- frontend/portfolio/public/test/test.js
+++ frontend/portfolio/public/test/test.js
@@ -1,5 +1,9 @@
-    QUnit.test( "a basic test example", function( assert ) {
-      var value = "hello";
-      assert.equal( value, "hello", "We expect value to be hello" );
-    });
+QUnit.test( "footer loads", function( assert ) {
+  // var link = document.getElementById("qunit-fixture").getElementById("footer");
+  var link = document.getElementById("footer");
+
+  assert.equal(link.innerHTML, "");
+  Footer.init();
+  assert.equal(link.innerHTML, "");
+});
 
