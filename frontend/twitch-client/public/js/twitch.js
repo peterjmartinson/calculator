95835881b2125b66877c02c9277ca8ff5b17db23
Peter Martinson <peter.j.martinson@gmail.com>
2017-05-23 22:22:34 -0400

    See if a channel's streaming, get link to its page
    Includes all tests!

--- frontend/twitch-client/public/js/twitch.js
+++ frontend/twitch-client/public/js/twitch.js
@@ -1,12 +1,28 @@
 var twitch = {};
 
-twitch.fetchJSON = function(callback) {
-  var url = 'https://wind-bow.glitch.me/twitch-api/streams/freecodecamp?callback=?';
-
-  $.getJSON(url, function(data) {
-    console.log(data);
+twitch.fetchJSON = function(channel, callback) {
+  var url = 'https://wind-bow.glitch.me/twitch-api/streams/';
+  var suffix = channel + '?callback=?';
+  $.getJSON(url+suffix, function(data) {
     callback(data);
   });
-  // return true;
-}
+};
 
+/**
+ * determines whether or not a channel is streaming
+ * @params {object} object - a response object from TwitchTV API
+ * @returns {number} = 1 if streaming, = 2 if not streaming
+ *
+ * usage: twitch.fetchJSON('<channel>', twitch.isStreaming)
+*/
+twitch.isStreaming = function(object) {
+  if (object.stream) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+twitch.getLink = function(object) {
+  return object._links.channel;
+}
--- frontend/twitch-client/test/twitch.test.js
+++ frontend/twitch-client/test/twitch.test.js
@@ -9,9 +9,56 @@ q.test('There is a module', function(assert) {
 
 q.test('The AJAX call succeeds', function(assert) {
   var done = assert.async();
-  twitch.fetchJSON(function(res) {
+  var channel = 'freecodecamp';
+  twitch.fetchJSON(channel, function(res) {
     assert.notEqual(res, {}, 'the response is not empty');
     assert.equal(typeof res, 'object', 'there was a response');
+    done();
+  });
+});
+
+q.module('isStreaming()');
+q.test('function exists', function(assert) {
+  assert.equal(typeof twitch.isStreaming, 'function', 'isStreaming() is a function');
+});
+
+q.test('returns a number', function(assert) {
+  assert.equal(typeof twitch.isStreaming({}), 'number', 'isStreaming() returns a number');
+});
+
+q.test('recognizes a streaming channel', function(assert) {
+  var done = assert.async();
+  var channel = 'ESL_SC2';
+  twitch.fetchJSON(channel, function(res) {
+    assert.equal(twitch.isStreaming(res), 1, channel + ' is streaming');
+    done();
+  });
+});
+
+q.test('recognizes a nonstreaming channel', function(assert) {
+  var done = assert.async();
+  var channel = 'freecodecamp';
+  twitch.fetchJSON(channel, function(res) {
+    assert.equal(twitch.isStreaming(res), 0, channel + ' is not streaming');
+    done();
+  });
+});
+
+q.module('getLink()');
+q.test('function exists', function(assert) {
+  assert.equal(typeof twitch.getLink, 'function', 'getLink() is a function');
+});
+
+q.test('returns a string', function(assert) {
+  var a = {_links : { channel : 'abc'}};
+  assert.equal(typeof twitch.getLink(a), 'string', 'getLink() returns a string');
+});
+
+q.test('returns a link', function(assert) {
+  var done = assert.async();
+  var channel = 'freecodecamp';
+  twitch.fetchJSON(channel, function(res) {
+    assert.equal(twitch.getLink(res).substring(0, 8), 'https://', 'getLink() returns ' + twitch.getLink(res));
     done();
   });
 });
