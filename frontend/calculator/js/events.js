4d00f0721534413bee349714517f079197035567
Peter Martinson <peter.j.martinson@gmail.com>
2017-06-14 08:01:57 -0400

    Log key presses
    First, the CSS got screwed up somewhere.  *But*, now keypresses get
    noticed by the DOM!
    
    1. put `onclick="function()"` into the <button> tag
    2. Load the .js file at the end of <body>
    3. Create the relevant function inside .js
    4. add the function as a method on `window`

--- frontend/calculator/js/events.js
+++ frontend/calculator/js/events.js
@@ -1,26 +1,23 @@
-(function() {
+window.onload = (function() {
   'use strict';
 
   var dom = {};
 
-  dom.clear        = { button : document.getElementById('clear')        , val : 'C' };
-  dom.one          = { button : document.getElementById('one')          , val : '1' };
-  dom.two          = { button : document.getElementById('two')          , val : '2' };
-  dom.three        = { button : document.getElementById('three')        , val : '3' };
-  dom.four         = { button : document.getElementById('four')         , val : '4' };
-  dom.five         = { button : document.getElementById('five')         , val : '5' };
-  dom.six          = { button : document.getElementById('six')          , val : '6' };
-  dom.seven        = { button : document.getElementById('seven')        , val : '7' };
-  dom.eight        = { button : document.getElementById('eight')        , val : '8' };
-  dom.nine         = { button : document.getElementById('nine')         , val : '9' };
-  dom.zero         = { button : document.getElementById('zero')         , val : '0' };
-  dom.plus         = { button : document.getElementById('plus')         , val : '+' };
-  dom.minus        = { button : document.getElementById('minus')        , val : '-' };
-  dom.times        = { button : document.getElementById('times')        , val : '*' };
-  dom.divide       = { button : document.getElementById('divide')       , val : '/' };
-  dom.root         = { button : document.getElementById('root')         , val : 'root' };
-  dom.decimalpoint = { button : document.getElementById('decimalpoint') , val : '.' };
-  dom.equals       = { button : document.getElementById('equals')       , val : '=' };
-  dom.plus_minus   = { button : document.getElementById('plus-minus')   , val : 'pm' };
+  var buttons = document.getElementsByClassName('calc-button');
+
+  function respondToClick(res) {
+    console.log(res);
+  }
+
+
+  function key(b) {
+    console.log("keypressed: " + b);
+  }
+
+  // need the following to register the function
+  // the HTML only runs functions attached to the window
+  window.key = key;
+
+  
 
 }());
