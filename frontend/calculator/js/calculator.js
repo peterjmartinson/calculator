a31628fb8f2887fd101c4a50f35ea43a6b07564f
Peter Martinson <peter.j.martinson@gmail.com>
2017-06-15 21:14:58 -0400

    Implement tests, start copying functions

--- frontend/calculator/js/calculator.js
+++ frontend/calculator/js/calculator.js
@@ -11,19 +11,56 @@
     screen     : '0',
   };
 
-  // TRIM ALL VALUES TO 10 CHARACTERS OR LESS
+  /**
+   * Trim all values to 10 characters or less
+   *
+   * @params {string, number} A decimal number
+   * @returns {string} A <10 character string
+  */
   function trim(num) {
+     console.log(num);
      var numLen, truncLen, tempVal;
-
      numLen = num.toString().length;
      truncLen = (Math.trunc(Number(num))).toString().length;
      if (numLen === truncLen && numLen > 10) {
         num = 'ERROR';
      } else if (numLen > truncLen) {
-        tempVal = (Math.round(Number(num) * Math.pow(10, (10 - truncLen)))) / Math.pow(10, (10 - truncLen));
+        tempVal = (Math.round(Number(num) * Math.pow(10, (9 - truncLen)))) / Math.pow(10, (9 - truncLen));
         num = tempVal.toString();
      }
+     console.log(num);
      return num.toString();
   }
 
+  /**
+   * perform a single operation
+   *   pattern: A + B -> l o r
+   *
+   * @params {string} left of operator
+   * @params {string} operator
+   * @params {string} right of operator
+   * @returns {string}
+   */
+  function operate(l, o, r) {
+     l = Number(l);
+     r = Number(r);
+     if (o === '+') {
+        return trim((l + r).toString());
+     }
+     if (o === '-') {
+        return trim((l - r).toString());
+     }
+     if (o === '*') {
+        return trim((l * r).toString());
+     }
+     if (o === '/') {
+        if (r === 0) {
+           return 'DIV BY 0';
+        }
+        return trim((l / r).toString());
+     }
+  }
+
+  window.trim = trim;
+  window.operate = operate;
 }());
--- frontend/calculator/test/events.test.js
+++ frontend/calculator/test/events.test.js
@@ -11,3 +11,23 @@ q.test('It returns a value', function(assert) {
   assert.equal(window.keyStroke(3), 3, 'it returns 3');
 });
 
+q.module('trim()');
+q.test('It exists', function(assert) {
+  assert.equal(typeof window.trim, 'function', 'trim is a function on window');
+});
+
+q.test('It returns a string', function(assert) {
+  assert.equal(typeof window.trim(1), 'string', 'trim returns a string');
+});
+
+q.test('It returns a string of less than 10 characters', function(assert) {
+  var a = [1.11111111, 1.111111111, 1.1111111111, 1.11111111111, 1.111111111111];
+  for (let i = 0; i < a.length; i++) {
+    assert.equal(window.trim(a[i]).length, 10);
+  }
+});
+
+q.module('operate()');
+q.test('It exists', function(assert) {
+  assert.equal(typeof window.operate, 'function', 'operate is a function on window');
+});
