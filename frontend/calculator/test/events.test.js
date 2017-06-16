e6591fc5b694849217cdb560bfec223185d75ab9
Peter Martinson <peter.j.martinson@gmail.com>
2017-06-16 17:06:23 -0400

    Complete calState tests
    Well, this function does what it's supposed to do (determine
    calculator's state based on what's in all the registers).  But, it may
    be a poor implementation of this idea...

--- frontend/calculator/test/events.test.js
+++ frontend/calculator/test/events.test.js
@@ -41,3 +41,65 @@ q.test('It performs the correct calculations', function(assert) {
   assert.equal(window.operate(1, '/', 0), 'DIV BY 0', 'DIV BY 0');
   assert.equal(window.operate('j', '/', 's'), 'NaN', 'NaN');
 });
+
+q.module('calState()');
+q.test('It exists', function(assert) {
+  assert.equal(typeof window.calState, 'function', 'calState is a function on window');
+});
+q.test('It returns the correct states', function(assert) {
+  var A = 'empty',
+      B = 'empty',
+      C = 'empty',
+      oA = 'empty',
+      oB = 'empty';
+  assert.equal(window.calState(A, B, C, oA, oB), 1, 'state 1');
+  A = 'empty';
+  B = 'empty';
+  C = 'empty';
+  oA = '1';
+  oB = 'empty';
+  assert.equal(window.calState(A, B, C, oA, oB), 2, 'state 2');
+  A = '1';
+  B = '1';
+  C = 'empty';
+  oA = '1';
+  oB = 'empty';
+  assert.equal(window.calState(A, B, C, oA, oB), 3, 'state 3');
+  A = '1';
+  B = '1';
+  C = 'empty';
+  oA = '1';
+  oB = '1';
+  assert.equal(window.calState(A, B, C, oA, oB), 4, 'state 4');
+  A = '1';
+  B = '1';
+  C = '1';
+  oA = '1';
+  oB = '1';
+  assert.equal(window.calState(A, B, C, oA, oB), 5, 'state 5');
+  A = 'DIV BY 0';
+  B = 'empty';
+  C = 'empty';
+  oA = 'empty';
+  oB = 'empty';
+  assert.equal(window.calState(A, B, C, oA, oB), 6, 'state 6');
+  A = 'empty';
+  B = 'DIV BY 0';
+  C = 'empty';
+  oA = 'empty';
+  oB = 'empty';
+  assert.equal(window.calState(A, B, C, oA, oB), 6, 'state 6');
+  A = 'ERROR';
+  B = 'empty';
+  C = 'empty';
+  oA = 'empty';
+  oB = 'empty';
+  assert.equal(window.calState(A, B, C, oA, oB), 6, 'state 6');
+  A = 'empty';
+  B = 'ERROR';
+  C = 'empty';
+  oA = 'empty';
+  oB = 'empty';
+  assert.equal(window.calState(A, B, C, oA, oB), 6, 'state 6');
+});
+  
