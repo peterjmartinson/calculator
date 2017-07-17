1e9ac9d4cce8e389f02f42bb752c5f95e88723ad
Peter Martinson <peter.j.martinson@gmail.com>
2017-07-17 17:07:39 -0400

    Take input from keyboard
    This is needed to test the program.  I don't know how to do <shift>-=
    yet, so just using letters to denote operations at this point (e.g. 'p'
    = +).

--- frontend/calculator/js/events.js
+++ frontend/calculator/js/events.js
@@ -6,10 +6,86 @@ window.onload = (function() {
     return label
   }
 
+  // document.addEventListener('keydown', keyDownHandler, 0);
+  // document.addEventListener('keyup', keyUpHandler, 0);
+  document.addEventListener('keydown', keyHandler, 0);
+
+  function keyDownHandler(key) {
+    console.log("keydown: " + key.keyCode);
+  }
+
+  function keyUpHandler(key) {
+    console.log("keydown: " + key.keyCode);
+  }
+
+  function getKeyName(key) {
+    // 187: =
+    // p: 80
+    // m: 77
+    // d: 68
+    // t: 49
+    // console.log('keyCode: ' + key.keyCode);
+    let button = '';
+    switch(key.keyCode) {
+      case 187:
+        return '=';
+        break;
+      case 80:
+        return '+';
+        break;
+      case 77:
+        return '-';
+        break;
+      case 68:
+        return '*';
+        break;
+      case 49:
+        return '/';
+        break;
+      case 48:
+        return '0';
+        break;
+      case 49:
+        return '1';
+        break;
+      case 50:
+        return '2';
+        break;
+      case 51:
+        return '3';
+        break;
+      case 52:
+        return '4';
+        break;
+      case 53:
+        return '5';
+        break;
+      case 54:
+        return '6';
+        break;
+      case 55:
+        return '7';
+        break;
+      case 56:
+        return '8';
+        break;
+      case 57:
+        return '9';
+        break;
+      default:
+        break;
+    }
+  }
+
+  function keyHandler(key) {
+    let key_name = getKeyName(key);
+    if ( key_name ) {
+      console.log(key_name);
+    }
+  }
+
   // need the following to register the function
   // the HTML only runs functions attached to the window
   window.keyStroke = keyStroke;
-
-  
 
 }());
