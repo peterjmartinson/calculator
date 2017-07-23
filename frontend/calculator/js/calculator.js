3f21e6e6d8b30cad53e4a7f33bb2896181ac6a49
Peter Martinson <peter.j.martinson@gmail.com>
2017-07-22 21:55:53 -0400

    Remove scattered `toString`; Improve tests
    Entries are passed to the buffer as strings.  Changes to and from
    numbers are done in the operator functions.
    
    Duplication was reduced in the tests.

--- frontend/calculator/js/calculator.js
+++ frontend/calculator/js/calculator.js
@@ -11,7 +11,6 @@ var Calculator = function() {
 
   let cowport = document.getElementById("cowport");
   let screen = document.getElementById("screen");
-  let entered = '';
   const key_map = {
       187: '=',
       80: '+',
@@ -32,12 +31,14 @@ var Calculator = function() {
   }
 
 
-  function getEntered() {
-    return entered;
+  let Entry = '';
+
+  function getEntry() {
+    return Entry;
   }
 
-  function setEntered(new_entered) {
-    entered = new_entered;
+  function setEntry(new_Entry) {
+    Entry = new_Entry.toString();
   }
 
   document.addEventListener('keyup', keyHandler, 0);
@@ -53,7 +54,7 @@ var Calculator = function() {
   }
 
   function keyHandler(key) {
-    setEntered(key_map[key.keyCode]);
+    setEntry(key_map[key.keyCode]);
     if ( key.keyCode >= 48 && key.keyCode <= 57 ) {
       setNumber();
     } else
@@ -82,12 +83,6 @@ var Calculator = function() {
 
   
 
-  /**
-   * Trim all values to 10 characters or less
-   *
-   * @params {string, number} A decimal number
-   * @returns {string} A <10 character string
-  */
   function trim(num) {
      var numLen, truncLen, tempVal;
      numLen = num.toString().length;
@@ -101,12 +96,32 @@ var Calculator = function() {
      return num.toString();
   }
 
+  function operate(l, o, r) {
+     l = Number(l);
+     r = Number(r);
+     if (o === '+') {
+        return trim((l + r));
+     }
+     if (o === '-') {
+        return trim((l - r));
+     }
+     if (o === '*') {
+        return trim((l * r));
+     }
+     if (o === '/') {
+        if (r === 0) {
+           return 'DIV BY 0';
+        }
+        return trim((l / r));
+     }
+  }
+
   function reckonInside() {
     if (buffer.register_c === 'empty') {
       buffer.register_c = buffer.register_b
     }
     let result = operate(buffer.register_b, buffer.operator_b, buffer.register_c);
-    buffer.register_b = result;
+    buffer.register_b = result.toString();
     buffer.screen_flag = 2;
   }
 
@@ -115,27 +130,16 @@ var Calculator = function() {
       buffer.register_b = buffer.register_a
     }
     let result = operate(buffer.register_a, buffer.operator_a, buffer.register_b);
-    buffer.register_a = result;
+    buffer.register_a = result.toString();
   }
 
-  function operate(l, o, r) {
-     l = Number(l);
-     r = Number(r);
-     if (o === '+') {
-        return trim((l + r).toString());
-     }
-     if (o === '-') {
-        return trim((l - r).toString());
-     }
-     if (o === '*') {
-        return trim((l * r).toString());
-     }
-     if (o === '/') {
-        if (r === 0) {
-           return 'DIV BY 0';
-        }
-        return trim((l / r).toString());
-     }
+  function equal() {
+    if (buffer.register_b != 'empty' && buffer.register_c != 'empty' && buffer.operator_b != 'empty') {
+      reckonInside();
+      reckonOutside();
+    } else {
+      reckonOutside();
+    }
   }
 
   /*
@@ -170,20 +174,16 @@ var Calculator = function() {
   function updateScreen() {
     if (buffer.screen_flag === 1) {
        if (buffer.register_a === 'empty') {
-          // this.screen = '0';
           screen.innerHTML = '0';
        } else {
-          // this.screen = this.regA;
-          screen.innerHTML = buffer.register_a.toString();
+          screen.innerHTML = buffer.register_a;
        }
     }
     if (buffer.screen_flag === 2) {
-       // this.screen = this.regB;
-       screen.innerHTML = buffer.register_b.toString();
+       screen.innerHTML = buffer.register_b;
     }
     if (buffer.screen_flag === 3) {
-       // this.screen = this.regC;
-       screen.innerHTML = buffer.register_c.toString();
+       screen.innerHTML = buffer.register_c;
     }
   }
 
@@ -202,34 +202,34 @@ var Calculator = function() {
       case 1:
         buffer.screen_flag = 1;
         if (buffer.register_a === 'empty' || buffer.register_a === '0') {
-          buffer.register_a = getEntered().toString();
-        } else if (buffer.register_a.toString().length < 10) {
-          buffer.register_a = buffer.register_a.toString() + getEntered();
+          buffer.register_a = getEntry();
+        } else if (buffer.register_a.length < 10) {
+          buffer.register_a = buffer.register_a + getEntry();
         }
         break;
       case 2:
         buffer.screen_flag = 2;
         if (buffer.register_b === 'empty' || buffer.register_b === '0') {
-          buffer.register_b = getEntered().toString();
-        } else if (buffer.register_b.toString().length < 10) {
-          buffer.register_b = buffer.register_b.toString() + getEntered();
+          buffer.register_b = getEntry();
+        } else if (buffer.register_b.length < 10) {
+          buffer.register_b = buffer.register_b + getEntry();
         }
         break;
       case 3:
         if (buffer.register_b === 'empty' || buffer.register_b === '0') {
-          buffer.register_b = getEntered().toString();
-        } else if (buffer.register_b.toString().length < 10) {
-          buffer.register_b = buffer.register_b.toString() + getEntered();
+          buffer.register_b = getEntry();
+        } else if (buffer.register_b.length < 10) {
+          buffer.register_b = buffer.register_b + getEntry();
         }
         buffer.screen_flag = 2;
         break;
       case 4:
-         buffer.register_c = getEntered().toString();
+         buffer.register_c = getEntry();
          buffer.screen_flag = 3;
          break;
       case 5:
-         if (buffer.register_c.toString().length < 10) {
-            buffer.register_c = buffer.register_c.toString() + getEntered();
+         if (buffer.register_c.length < 10) {
+            buffer.register_c = buffer.register_c + getEntry();
             buffer.screen_flag = 3;
          }
          break;
@@ -239,57 +239,56 @@ var Calculator = function() {
          console.log("something other than NUMBER happened!");
          break;
     }
-    setState();
   }
 
   function setOperator() {
     switch (buffer.state) {
       case 1:
-         buffer.operator_a = getEntered();
+         buffer.operator_a = getEntry();
          buffer.screen_flag = 1;
          break;
       case 2:
-         buffer.operator_a = getEntered();
+         buffer.operator_a = getEntry();
          buffer.screen_flag = 1;
          break;
       case 3:
-         if ((buffer.operator_a === '+' || buffer.operator_a === '-') && (getEntered() === '*' || getEntered() === '/')) {
-            buffer.operator_b = getEntered();
+         if ((buffer.operator_a === '+' || buffer.operator_a === '-') && (getEntry() === '*' || getEntry() === '/')) {
+            buffer.operator_b = getEntry();
             buffer.screen_flag = 2;
          } else {
             buffer.register_a = operate(buffer.register_a, buffer.operator_a, buffer.register_b);
             buffer.register_b = 'empty';
-            buffer.operator_a = getEntered();
+            buffer.operator_a = getEntry();
             buffer.screen_flag = 1;
          }
          break;
       case 4:
-         if (getEntered() === '+' || getEntered() === '-') {
+         if (getEntry() === '+' || getEntry() === '-') {
             buffer.register_a = operate(buffer.register_a, buffer.operator_a,
                operate(buffer.register_b, buffer.operator_b, buffer.register_b));
             buffer.register_b = 'empty';
             buffer.register_c = 'empty';
-            buffer.operator_a = getEntered();
+            buffer.operator_a = getEntry();
             buffer.operator_b = 'empty';
             buffer.screen_flag = 1;
          } else {
-            buffer.operator_b = getEntered();
+            buffer.operator_b = getEntry();
             buffer.screen_flag = 2;
          }
          break;
       case 5:
-         if (getEntered() === '+' || getEntered() === '-') {
+         if (getEntry() === '+' || getEntry() === '-') {
             buffer.register_a = operate(buffer.register_a, buffer.operator_a,
                operate(buffer.register_b, buffer.operator_b, buffer.register_c));
             buffer.register_b = 'empty';
             buffer.register_c = 'empty';
-            buffer.operator_a = getEntered();
+            buffer.operator_a = getEntry();
             buffer.operator_b = 'empty';
             buffer.screen_flag = 1;
          } else {
             buffer.register_b = operate(buffer.register_b, buffer.operator_b, buffer.register_c);
             buffer.register_c = 'empty';
-            buffer.operator_b = getEntered();
+            buffer.operator_b = getEntry();
             buffer.screen_flag = 2;
          }
          break;
@@ -299,17 +298,8 @@ var Calculator = function() {
          console.log("something other than OPERATOR happened!");
          break;
     }
-    setState();
   }
 
-  function equal() {
-    if (buffer.register_b != 'empty' && buffer.register_c != 'empty' && buffer.operator_b != 'empty') {
-      reckonInside();
-      reckonOutside();
-    } else {
-      reckonOutside();
-    }
-  }
   // +/-
   // .
   // square root
@@ -327,8 +317,8 @@ var Calculator = function() {
     reckonInside  : reckonInside,
     reckonOutside : reckonOutside,
     equal         : equal,
-    getEntered    : getEntered,
-    setEntered    : setEntered
+    getEntry      : getEntry,
+    setEntry      : setEntry
   };
 
 
--- frontend/calculator/test/events.test.js
+++ frontend/calculator/test/events.test.js
@@ -1,8 +1,11 @@
-var q = QUnit;
+/*jshint esversion:6, node:true */
+'use strict';
+
+const q = QUnit;
 
 q.module('operations');
 
-q.test('There is a module', function(assert) {
+q.test('exist', function(assert) {
   assert.equal(typeof calculator.operate, 'function', 'operate is a function');
   assert.equal(typeof calculator.reckonInside, 'function', 'reckonInside is a function');
   assert.equal(typeof calculator.reckonOutside, 'function', 'reckonOutside is a function');
@@ -74,23 +77,23 @@ q.test('equal() performs the whole calculation', function(assert) {
 });
 
 q.module('keyStroke()');
-q.test('There is a module', function(assert) {
+q.test('exists', function(assert) {
   assert.equal(typeof window.keyStroke, 'function', 'keyStroke is a function');
 });
-q.test('It returns a value', function(assert) {
+q.test('returns a value', function(assert) {
   assert.equal(window.keyStroke('clear'), 'clear', 'it returns `clear`');
   assert.equal(window.keyStroke('pm'), 'pm', 'it returns `pm`');
   assert.equal(window.keyStroke(3), 3, 'it returns 3');
 });
 
 q.module('trim()');
-q.test('It exists', function(assert) {
+q.test('exists', function(assert) {
   assert.equal(typeof calculator.trim, 'function', 'trim is a function on C');
 });
-q.test('It returns a string', function(assert) {
+q.test('returns a string', function(assert) {
   assert.equal(typeof calculator.trim(1), 'string', 'trim returns a string');
 });
-q.test('It returns a string of less than 10 characters', function(assert) {
+q.test('returns a string of less than 10 characters', function(assert) {
   var a = [1.11111111, 1.111111111, 1.1111111111, 1.11111111111, 1.111111111111];
   for (let i = 0; i < a.length; i++) {
     assert.equal(calculator.trim(a[i]).length, 10);
@@ -98,63 +101,54 @@ q.test('It returns a string of less than 10 characters', function(assert) {
 });
 
 q.module('setState()');
-q.test('It exists', function(assert) {
+q.test('exists', function(assert) {
   assert.equal(typeof calculator.setState, 'function', 'setState is a function on C');
 });
-q.test('It returns the correct states', function(assert) {
+q.test('returns the correct states', function(assert) {
 
   setBuffer(['empty','empty','empty','empty','empty']);
-  calculator.setState();
   assert.equal(calculator.buffer.state, 1, 'state 1');
 
   setBuffer(['empty', 'empty', 'empty', '+', 'empty']);
-  calculator.setState();
   assert.equal(calculator.buffer.state, 2, 'state 2');
 
   setBuffer(['1', '1', 'empty', '1', 'empty']);
-  calculator.setState(calculator.buffer);
   assert.equal(calculator.buffer.state, 3, 'state 3');
 
   setBuffer(['1', '1', 'empty', '1', '1']);
-  calculator.setState(calculator.buffer);
   assert.equal(calculator.buffer.state, 4, 'state 4');
 
   setBuffer(['1', '1', '1', '1', '1']);
-  calculator.setState(calculator.buffer);
   assert.equal(calculator.buffer.state, 5, 'state 5');
 
   setBuffer(['DIV BY 0', 'empty', 'empty', 'empty', 'empty']);
-  calculator.setState(calculator.buffer);
   assert.equal(calculator.buffer.state, 6, 'state 6');
 
   setBuffer(['empty', 'DIV BY 0', 'empty', 'empty', 'empty']);
-  calculator.setState(calculator.buffer);
   assert.equal(calculator.buffer.state, 6, 'state 6');
 
   setBuffer(['ERROR', 'empty', 'empty', 'empty', 'empty']);
-  calculator.setState(calculator.buffer);
   assert.equal(calculator.buffer.state, 6, 'state 6');
 
   setBuffer(['empty', 'ERROR', 'empty', 'empty', 'empty']);
-  calculator.setState(calculator.buffer);
   assert.equal(calculator.buffer.state, 6, 'state 6');
 });
 
 q.module('clear');
-q.test('It exists', function(assert) {
+q.test('exists', function(assert) {
   assert.equal(typeof calculator.clear, 'function', 'clear is a function on Calculator');
 });
-q.test('It returns the correct states', function(assert) {
+q.test('resets the buffer', function(assert) {
   setBuffer(['1','1','1','+','*',2,'0',1]);
   calculator.clear();
   assert.deepEqual(calculator.buffer, default_buffer, 'clear resets the buffer');
 });
 
 q.module('updateScreen');
-q.test('It exists', function(assert) {
+q.test('exists', function(assert) {
   assert.equal(typeof calculator.updateScreen, 'function', 'updateScreen is a function on Calculator');
 });
-q.test('It returns the correct states', function(assert) {
+q.test('returns the correct states', function(assert) {
   setBuffer(['1', '2', '3', '+', '*']);
 
   calculator.buffer.screen_flag = 1;
@@ -171,109 +165,105 @@ q.test('It returns the correct states', function(assert) {
 });
 
 q.module('setNumber');
-q.test('It exists', function(assert) {
+q.test('exists', function(assert) {
   assert.equal(typeof calculator.setNumber, 'function', 'setNumber is a function on Calculator');
 });
-q.test('The correct registers get filled', function(assert) {
-  calculator.setEntered('5');
+q.test('fills the correct registers', function(assert) {
+  calculator.setEntry('5');
 
   // state 1
   setBuffer(['empty','empty','empty','empty','empty']);
-  calculator.setState();
-  console.log(getResult());
   calculator.setNumber();
-  console.log(getResult());
   assert.ok(calculator.buffer.register_a == '5' && calculator.buffer.screen_flag == 1, 'state 1 updated correctly');
 
   setBuffer(['27','empty','empty','empty','empty']);
-  calculator.setState();
-  calculator.setNumber(number);
+  calculator.setNumber();
   assert.ok(calculator.buffer.register_a == '275' && calculator.buffer.screen_flag == 1, 'state 1 updated correctly - append');
 
   // state 2
   setBuffer(['1','empty','empty','+','empty']);
-  calculator.setState();
-  calculator.setNumber(number);
+  calculator.setNumber();
   assert.ok(calculator.buffer.register_b == '5' && calculator.buffer.screen_flag == 2, 'state 2 updated correctly');
 
   // state 3
   setBuffer(['1','1','empty','+','empty']);
-  calculator.setState();
-  calculator.setNumber(number);
+  calculator.setNumber();
   assert.ok(calculator.buffer.register_b == '15' && calculator.buffer.screen_flag == 2, 'state 3 updated correctly');
 
   // state 4
   setBuffer(['1','1','empty','+','*']);
-  calculator.setState();
-  calculator.setNumber(number);
+  calculator.setNumber();
   assert.ok(calculator.buffer.register_c == '5' && calculator.buffer.screen_flag == 3, 'state 4 updated correctly');
 
   // state 5
   setBuffer(['1','1','1','+','*']);
-  calculator.setState();
-  calculator.setNumber(number);
+  calculator.setNumber();
   assert.ok(calculator.buffer.register_c == '15' && calculator.buffer.screen_flag == 3, 'state 4 updated correctly');
 });
 
-// q.module('setOperator');
-// q.test('It exists', function(assert) {
-//   assert.equal(typeof calculator.setOperator, 'function', 'setOperator is a function on Calculator');
-// });
-// q.test('The correct registers get filled', function(assert) {
-//   var plus = '+';
-//   var times = '*';
-//   // state 1
-//   setBuffer(['empty','empty','empty','empty','empty']);
-//   calculator.setState();
-//   assert.equal(calculator.buffer.state, '1', 'State is correct');
-//   calculator.setOperator(plus);
-//   assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 1: a');
-//   setBuffer(['27','empty','empty','empty','empty']);
-//   calculator.setState();
-//   calculator.setOperator(plus);
-//   assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 1: b');
-//   // state 2
-//   setBuffer(['27','empty','empty','+','empty']);
-//   calculator.setState();
-//   assert.equal(calculator.buffer.state, '2', 'State is correct');
-//   calculator.setOperator(plus);
-//   assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 2: a');
-//   setBuffer(['27','empty','empty','*','empty']);
-//   calculator.setState();
-//   calculator.setOperator(plus);
-//   assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 2: b');
-//   // state 3
-//   setBuffer(['27','2','empty','+','empty']);
-//   calculator.setState();
-//   assert.equal(calculator.buffer.state, '3', 'State is correct');
-//   calculator.setOperator(plus);
-//   assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 3: a');
-//   setBuffer(['27','2','empty','+','empty']);
-//   calculator.setState();
-//   calculator.setOperator(times);
-//   assert.ok(calculator.buffer.operator_b == '*' && calculator.buffer.screen_flag == 2, 'state 3: b');
-//   // state 4
-//   setBuffer(['27','2','empty','+','+']);
-//   calculator.setState();
-//   assert.equal(calculator.buffer.state, '4', 'State is correct');
-//   calculator.setOperator(plus);
-//   assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 4: a');
-//   setBuffer(['27','2','empty','+','*']);
-//   calculator.setState();
-//   assert.equal(calculator.buffer.state, '4', 'State is correct');
-//   calculator.setOperator(plus);
-//   assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 4: b');
-//   setBuffer(['27','2','empty','+','+']);
-//   calculator.setState();
-//   assert.equal(calculator.buffer.state, '4', 'State is correct');
-//   calculator.setOperator(times);
-//   assert.ok(calculator.buffer.operator_b == '*' && calculator.buffer.screen_flag == 2, 'state 4: a');
-//   setBuffer(['27','2','empty','+','*']);
-//   calculator.setState();
-//   assert.equal(calculator.buffer.state, '4', 'State is correct');
-//   calculator.setOperator(times);
-//   assert.ok(calculator.buffer.operator_b == '*' && calculator.buffer.screen_flag == 2, 'state 4: b');
-// });
+q.module('setOperator');
+q.test('exists', function(assert) {
+  assert.equal(typeof calculator.setOperator, 'function', 'setOperator is a function on Calculator');
+});
+
+q.test('fills the correct registers', function(assert) {
+  let plus = '+',
+      times = '*';
+
+  // state 1
+  calculator.setEntry(plus);
+  setBuffer(['empty','empty','empty','empty','empty']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 1: a');
+
+  calculator.setEntry(plus);
+  setBuffer(['27','empty','empty','empty','empty']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 1: b');
+
+  // state 2
+  calculator.setEntry(plus);
+  setBuffer(['27','empty','empty','+','empty']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 2: a');
+
+  calculator.setEntry(plus);
+  setBuffer(['27','empty','empty','*','empty']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 2: b');
+
+  // state 3
+  calculator.setEntry(plus);
+  setBuffer(['27','2','empty','+','empty']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 3: a');
+
+  calculator.setEntry(times);
+  setBuffer(['27','2','empty','+','empty']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_b == '*' && calculator.buffer.screen_flag == 2, 'state 3: b');
+
+  // state 4
+  calculator.setEntry(plus);
+  setBuffer(['27','2','empty','+','+']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 4: a');
+
+  calculator.setEntry(plus);
+  setBuffer(['27','2','empty','+','*']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_a == '+' && calculator.buffer.screen_flag == 1, 'state 4: b');
+
+  calculator.setEntry(times);
+  setBuffer(['27','2','empty','+','+']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_b == '*' && calculator.buffer.screen_flag == 2, 'state 4: a');
+
+  calculator.setEntry(times);
+  setBuffer(['27','2','empty','+','*']);
+  calculator.setOperator();
+  assert.ok(calculator.buffer.operator_b == '*' && calculator.buffer.screen_flag == 2, 'state 4: b');
+});
 
 
 
@@ -289,18 +279,12 @@ let setBuffer = function(buffer) {
   calculator.buffer.screen_flag = buffer[5]; 
   calculator.buffer.screen      = buffer[6]; 
   calculator.buffer.state       = buffer[7]; 
+  calculator.setState();
 }
 
 let getResult = function() {
-  return [calculator.buffer.register_a, calculator.buffer.register_b, calculator.buffer.register_c, calculator.buffer.operator_a, calculator.buffer.operator_b]
+  return [calculator.buffer.register_a, calculator.buffer.register_b, calculator.buffer.register_c, calculator.buffer.operator_a, calculator.buffer.operator_b];
 }
-
-
-
-
-
-
-
 
 let default_buffer = {
   register_a  : 'empty',
